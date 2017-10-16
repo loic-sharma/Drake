e36efb3df89cd44ca0a8bb3959d51bf192846e15
Loïc Sharma <sharma.loic@gmail.com>
2017-10-15 20:59:29 -0700

    Added REST endpoint

---
+++ src/Drake.Api/Controllers/RepositoryController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Drake.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Drake.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class RepositoryController : Controller
+    {
+		private static readonly Uri GitHubUri = new Uri("https://github.com/");
+
+        private DrakeContext _db;
+
+        public RepositoryController(DrakeContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        // GET api/repository/{owner}/{repository}
+        [HttpGet("{owner}/{repository}")]
+        public async Task<object> Get(string owner, string repositoryName)
+        {
+            var uri = new Uri(GitHubUri, $"{owner}/{repositoryName}.git").ToString();
+            var repository = await _db.Repositories
+                                      .Where(r => r.RepositoryUri == uri)
+                                      .Include(r => r.Files)
+                                      .FirstAsync();
+
+            return new
+            {
+				repository.RepositoryId,
+                repository.RepositoryUri,
+                repository.LastPullTime,
+                Files = repository.Files.Select(f => new
+                {
+                    f.FileId,
+                    f.Path,
+                    f.Commits,
+                }),
+            };
+        }
+    }
+}
